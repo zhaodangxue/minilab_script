2576af3e71ec42a3d3b36b9abc366290e9370b3d
zhaodangxue <z404583637@sjtu.edu.cn>
2024-07-08 10:34:54 +0800

    fix script

--- test_script.py
+++ test_script.py
@@ -6,19 +6,10 @@ def log(s, col="green"):
 def main():
     log("copy test code from github")
     os.system("git clone https://github.com/zhaodangxue/mininetLab.git")
-    python3 = "sudo python3 "
+    python3 = "python3 "
     log("run test code")
     os.chdir("mininetLab")
-    os.system("mkdir data")
-    os.system(python3 + "test_spine_leaf_convergence.py")
-    os.system(python3 + "test_spine_leaf_reconnected.py")
-    os.system(python3 + "test_spine_leaf_tcp_1Gbps.py")
-    os.system(python3 + "test_spine_leaf_tcp_200Mbps.py")
-    os.system(python3 + "test_spine_leaf_tcp_send_h1self.py")
-    os.system(python3 + "test_spine_leaf_udp_send_h1self.py")
-    os.system(python3 + "test_spine_leaf_udp_1Gbps.py")
-    os.system(python3 + "test_spine_leaf_udp_300Mbps.py")
-    os.system(python3 + "test_spine_leaf_udp_100Mbps.py")
+    os.system(python3 + "test_script.py")
     os.system("mv ./data ../data")
     os.chdir("..")
     os.system("rm -rf mininetLab")
